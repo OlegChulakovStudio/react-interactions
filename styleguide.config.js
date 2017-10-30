3f8c5638925544999c20c995f124f50833ab5dcc
Shilov Vlad <vlad.shilov@chulakov.ru>
2017-10-30 20:12:09 +0300

    Add first demo

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,6 +1,11 @@
+const path = require('path');
+
 module.exports = {
   webpackConfig: require('./config/webpack.config.dev.js'),
   title: "react-interactions",
   showCode: true,
-  styleguideDir: "styleguide/build"
+  styleguideDir: "styleguide/build",
+  require: [
+    path.join(__dirname, 'styleguide/style.css')
+  ]
 }; 

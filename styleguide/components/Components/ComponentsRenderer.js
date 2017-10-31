ab617b9e62af49aadf4a21cacc0775ea611e8fd3
Shilov Vlad <vlad.shilov@chulakov.ru>
2017-10-31 19:25:57 +0300

    Update demo, ESlint fixes for styleguide files

--- styleguide/components/Components/ComponentsRenderer.js
+++ styleguide/components/Components/ComponentsRenderer.js
@@ -4,56 +4,96 @@ import Styled from 'react-styleguidist/lib/rsg-components/Styled';
 import { Logo, Heading } from 'chramework';
 
 const styles = ({ fontFamily, fontSize, color, mq }) => ({
-    root: {
-        wordBreak: 'break-word'
-    },
-    header: {
-        display: 'flex',
-        padding: [[15, 0]],
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginBottom: 30,
+	root: {
+		wordBreak: 'break-word'
+	},
+	header: {
+		display: 'flex',
+		padding: [[15, 0]],
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		marginBottom: 30,
 		[mq.small]: {
-            paddingTop: 0,
-            marginBottom: 20,
+			paddingTop: 0,
+			marginBottom: 20,
 		},
-    },
-    githubLink: {
+	},
+	githubLink: {
+		marginLeft: 20,
 		fontFamily: fontFamily.base,
-        fontSize: fontSize.base,
-        color: color.link,
+		fontSize: fontSize.base,
+		color: color.link,
 		'&:hover': {
-            fontFamily: fontFamily.base,
-            fontSize: fontSize.base,
-            color: color.linkHover,
+			marginLeft: 20,
+			fontFamily: fontFamily.base,
+			fontSize: fontSize.base,
+			color: color.linkHover,
 		},
-    },
-    promo: {
-        marginBottom: 90,
+	},
+	telegramLink: {
+		fontFamily: fontFamily.base,
+		fontSize: fontSize.base,
+		color: color.link,
+		'&:hover': {
+			fontFamily: fontFamily.base,
+			fontSize: fontSize.base,
+			color: color.linkHover,
+		},
 		[mq.small]: {
-            marginBottom: 55,
+			display: 'none',
 		},
-    },
+	},
+	promo: {
+		marginBottom: 90,
+		[mq.small]: {
+			marginBottom: 55,
+		},
+	},
+	heading: {
+		marginBottom: 10,
+	},
+	text: {
+		fontSize: fontSize.base,
+		fontFamily: fontFamily.base,
+		color: color.base,
+	},
+	copyright: {
+		marginTop: 90,
+		fontSize: fontSize.base,
+		fontFamily: fontFamily.base,
+		color: color.base
+	}
 });
 
 function ComponentsRenderer({ classes, children }) {
-    return (
-        <div className={classes.root}>
-            <div class={classes.promo}>
-                <header className={classes.header}>
-                    <Logo url="http://chulakov.ru/" external />
-                    <a className={classes.githubLink} href="https://github.com/OlegChulakovStudio/react-interactions" target="_blank">GitHub</a>
-                </header>
-                <Heading level={3} tagName="h1">react-interactions</Heading>
-            </div>
-            {children}
-        </div>
-    );
+	return (
+		<div className={classes.root}>
+			<div class={classes.promo}>
+				<header className={classes.header}>
+					<Logo url="http://chulakov.ru/" external />
+					<div className={classes.links}>
+						<a className={classes.telegramLink} href="https://t.me/deepdigital" target="_blank">Telegram-channel (RUS)</a>
+						<a className={classes.githubLink} href="https://github.com/OlegChulakovStudio/react-interactions" target="_blank">GitHub Project</a>
+					</div>
+				</header>
+				<div className={classes.heading}>
+					<Heading level={3} tagName="h1">react-interactions</Heading>
+				</div>
+				<div className={classes.text}>
+					React component for micro-interactions
+				</div>
+			</div>
+			{children}
+			<div className={classes.copyright}>
+				&copy; Chulakov Digital
+			</div>
+		</div>
+	);
 };
 
 ComponentsRenderer.propTypes = {
-    children: PropTypes.node.isRequired,
+	children: PropTypes.node.isRequired,
 };
 
 export default Styled(styles)(ComponentsRenderer);
